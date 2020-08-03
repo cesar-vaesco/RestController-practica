03c406604379754868014c78246c170709cf370b
Cesar <cesarvargasescorcia@gmail.com>
2020-08-03 18:48:49 -0500

    Proyecto creado e inicializado desde método getProfesores en el controlador

---
+++ src/main/java/com/example/cursorest/dao/IProfesorDao.java
@@ -0,0 +1,23 @@
+package com.example.cursorest.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.cursorest.entity.Profesor;
+
+public interface IProfesorDao extends JpaRepository<Profesor, Long>{
+
+public Profesor findByEmail(String email);
+	
+	public Profesor findByEmailAndPassword(String email, String password);
+	
+	public Optional<Profesor> findById(Long id);
+	
+	@Query("select p from Profesor p where p.id=?1")
+	public Profesor findByIdSQL(Long id);
+	
+	
+	
+}
---
+++ src/main/java/com/example/cursorest/service/IProfesorService.java
@@ -0,0 +1,31 @@
+package com.example.cursorest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.cursorest.entity.Profesor;
+
+public interface IProfesorService {
+
+public List<Profesor> findAll();
+	
+	public void save(Profesor profesor);
+	
+	public Profesor findProfesor(Profesor profesor);
+	
+	public Profesor checkProfesorLogin(Profesor profesor);
+	
+	public void deleteProfesor(Profesor profesor);
+	
+	public Profesor uptadeProfesor(Profesor profesor);
+	
+	public Optional<Profesor> findProfesorById(Long profesor_id);
+	
+	public void deleteProfesor(Long id);
+	
+	public void deleteAllProfesor();
+	
+	public Profesor findById(Long id);
+	
+	public Profesor findByIdSQL(Long id);
+}
