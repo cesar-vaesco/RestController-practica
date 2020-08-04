4e3fe618d9bd5be424b0de464942d983eacd0166
Cesar <cesarvargasescorcia@gmail.com>
2020-08-04 13:58:44 -0500

    ProfesorController: creando el método post para guardar un profesor

--- src/main/java/com/example/cursorest/controller/ProfesorRestController.java
+++ src/main/java/com/example/cursorest/controller/ProfesorRestController.java
@@ -3,8 +3,13 @@ package com.example.cursorest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cursorest.entity.Profesor;
@@ -15,12 +20,22 @@ import com.example.cursorest.service.IProfesorService;
 public class ProfesorRestController {
 
 	@Autowired
-	private IProfesorService  profesorService;
-	
+	private IProfesorService profesorService;
+
 	@GetMapping("/profesores")
-	public List<Profesor> getProfesores(){
+	@ResponseStatus(HttpStatus.OK)
+	public List<Profesor> getProfesores() {
 		return profesorService.findAll();
 	}
-	
-	
+
+	@PostMapping("sign_up")
+	public ResponseEntity<Void> addProfesor(@RequestBody Profesor profesor) {
+		if (profesorService.findProfesor(profesor) == null) {
+			profesorService.save(profesor);
+			return new ResponseEntity<Void>(HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+	}
+
 }
