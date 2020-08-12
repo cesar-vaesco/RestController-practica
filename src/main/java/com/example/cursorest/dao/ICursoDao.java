37216a9caa5ef61414e74e33f906adc543420915
Cesar <cesarvargasescorcia@gmail.com>
2020-08-12 13:29:32 -0500

    Relación 1 a N: Creando la interfaz ICursoDao

---
+++ src/main/java/com/example/cursorest/dao/ICursoDao.java
@@ -0,0 +1,14 @@
+package com.example.cursorest.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cursorest.entity.Curso;
+
+public interface ICursoDao extends JpaRepository<Curso, Long> {
+
+	
+	public List<Curso> findByProfesorID(Long id);
+	
+}
