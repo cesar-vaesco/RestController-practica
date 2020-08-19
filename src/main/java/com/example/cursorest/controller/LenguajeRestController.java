c1aadb767eadf59892348e99809bc7fbe26a34f7
Cesar <cesarvargasescorcia@gmail.com>
2020-08-19 14:52:27 -0500

    Relación m a m: creando lenguaje controller - POST: crear un nuevo lenguaje

--- src/main/java/com/example/cursorest/controller/LenguajeRestController.java
+++ src/main/java/com/example/cursorest/controller/LenguajeRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,14 @@ public class LenguajeRestController {
 			
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 	}
+	
+	@PostMapping("/crear-lenguaje")
+	public ResponseEntity<?> agregarLenguaje(@RequestBody Lenguaje lenguaje){
+		lenguajeService.saveLenguaje(lenguaje);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+	}
+	
+	
+	
 	
 }
--- src/main/java/com/example/cursorest/dao/ILenguajeDao.java
+++ src/main/java/com/example/cursorest/dao/ILenguajeDao.java
@@ -2,7 +2,6 @@ package com.example.cursorest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import com.example.cursorest.entity.Lenguaje;
 
